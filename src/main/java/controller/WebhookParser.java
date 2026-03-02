f91fafe73e765d60ab310cdef16192ce0db7df12
NGL321 <noahlitov@gmail.com>
2026-03-01 22:57:31 -0800

    feat: Backend project structure and placeholder files

---
+++ src/main/java/controller/WebhookParser.java
@@ -0,0 +1,4 @@
+class WebhookParser {
+    // TODO: Placeholder for backend controller layer.
+    // TODO: Parse messages from webhook into commands.
+}
---
+++ src/main/java/model/Simulation.java
@@ -0,0 +1,4 @@
+class Simulation {
+    // TODO: Placeholder for simulation logic
+    // TODO: Execute transition function and manage sim state
+}
